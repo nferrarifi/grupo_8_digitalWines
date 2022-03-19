acac64de0ea689d326a314c6745b840d73a77c37
MauroCarb <mauro.carbajales@gmail.com>
2022-03-19 20:04:41 -0300

    Prettier de Index.html, app.js, y styles.css

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const express = require ("express")
-const app = express()
-const path = require ("path")
+const express = require("express");
+const app = express();
+const path = require("path");
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.listen (3050, () => {
-    console.log ("servidor corriendo en el puerto 3000")
+app.listen(3050, () => {
+  console.log("servidor corriendo en el puerto 3000");
+});
 
-})
-
-app.get ("/", (req,res) => {
-    res.sendFile(path.resolve("./views/index.html"))
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("./views/index.html"));
+});
