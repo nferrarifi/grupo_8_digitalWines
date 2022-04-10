44f68d05378c0e268d40d61366f93647c5f037fd
nferrarifi <nahuel.ferrarifisicaro@gmail.com>
2022-04-10 18:46:42 -0300

    EJS, Rutas, controladores y pagina de creacion de producto

--- routers/main.js
+++ routers/main.js
@@ -8,17 +8,17 @@ router.get("/", mainControllers.index);
 
   router.get("/productdetail/:id?", mainControllers.productDetail);
   
-  router.get("/productCart", (req, res) => {
-    res.sendFile(path.resolve("./views/productCart.html"));
-  });
-  router.get("/login", (req, res) => {
-    res.sendFile(path.resolve("./views/login.html"));
-  });
-  router.get("/register", (req, res) => {
-    res.sendFile(path.resolve("./views/register.html"));
-  });
-  router.get("/about-us", (req, res) => {
-    res.sendFile(path.resolve("./views/about-us.html"));
-  });
+  router.get("/productCart", mainControllers.productCart  )
+
+  router.get("/newProduct", mainControllers.newProduct)
+
+  router.get("/login", mainControllers.login)
+
+
+  router.get("/register", mainControllers.register)
+
+
+  router.get("/about-us", mainControllers.about)
+
 
   module.exports = router
