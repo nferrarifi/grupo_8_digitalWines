779dfb3ee4ce5d76ffd122121a3b14a33751e546
alejandro Coral <alejandrocorald@gmail.com>
2022-05-02 22:20:05 -0500

    post productos

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,90 +1,85 @@
-const express = require ("express")
-const fs = require ("fs")
-const path = require ("path")
-const productsFilePath = path.join(__dirname, '../data/products.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-const destacados = products.filter ((vino) => vino.destacado === true)
-
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const productsFilePath = path.join(__dirname, "../data/products.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
+const destacados = products.filter((vino) => vino.destacado === true);
 
 const mainControllers = {
+  index: (req, res) => {
+    res.render("index", { destacados });
+  },
 
-index: (req,res) => {
-    res.render ('index', { destacados })
-},
+  products: (req, res) => {
+    let routeID = Number(req.params.id);
+    res.render("products/products", { products });
+  },
 
-products: (req,res) => {
-    let routeID = Number(req.params.id)
-    res.render ('products/products', { products })
-},
+  productDetail: (req, res) => {
+    let routeID = Number(req.params.id);
+    let product = products.find((p) => p.id == routeID);
+    product == undefined
+      ? res.send("Producto no encontrado")
+      : res.render("products/productdetail", { product: product });
+  },
 
-productDetail: (req,res) => {
-    let routeID = Number(req.params.id)
-    let product = products.find ((p) => p.id == routeID)
-    product == undefined ? res.send ("Producto no encontrado") : res.render ('products/productdetail', {"product" : product})
-    },
- 
-productCart: (req,res) => {
-    res.render ('products/productCart')
-},
+  productCart: (req, res) => {
+    res.render("products/productCart");
+  },
 
-login: (req,res) => {
-    res.render ('users/login')
-},
+  login: (req, res) => {
+    res.render("users/login");
+  },
 
-register: (req,res) => {
-    res.render ('users/register')
-},
+  register: (req, res) => {
+    res.render("users/register");
+  },
 
-about: (req,res) => {
-    res.render ('about-us')
-},
+  about: (req, res) => {
+    res.render("about-us");
+  },
 
-newProduct: (req,res) => {
-    res.render ('products/newProduct')
-},
-destroy : (req, res) => {
-    id=req.params.id
-    let aEliminar= products.filter((p)=> 
-    p.id != id
-    )
-    console.log(aEliminar)
+  newProduct: (req, res) => {
+    res.render("products/newProduct");
+  },
+  store: (req, res) => {
+    res.redirect("/products");
+  },
+  destroy: (req, res) => {
+    id = req.params.id;
+    let aEliminar = products.filter((p) => p.id != id);
+    console.log(aEliminar);
     fs.writeFileSync(productsFilePath, JSON.stringify(aEliminar), "utf-8");
 
-      res.redirect("/products");  
+    res.redirect("/products");
+  },
+  edit: (req, res) => {
+    let id = req.params.id;
+    let productToEdit = products.find((element) => element.id == id);
+    res.render("product-edit-form", { productToEdit });
+  },
 
-},
-edit: (req, res) => {
-    let id = req.params.id
-    let productToEdit = products.find(element => element.id == id);
-    res.render("product-edit-form", {productToEdit})
-},
-
-update: (req, res) => {
-    let id = req.params.id
-    let productToEdit = products.find(element => element.id == id);
-    let image ="red-wine-4813262_640.jpg";
+  update: (req, res) => {
+    let id = req.params.id;
+    let productToEdit = products.find((element) => element.id == id);
+    let image = "red-wine-4813262_640.jpg";
     productToEdit = {
-        ...req.body,
-        id: id,
-        image,
-      };
+      ...req.body,
+      id: id,
+      image,
+    };
 
-      let newEditProduct= products.map( (p)=>{
-          if(p.id == id){
-             return (p = { ...productToEdit});
-          }
-          return p;
+    let newEditProduct = products.map((p) => {
+      if (p.id == id) {
+        return (p = { ...productToEdit });
+      }
+      return p;
+    });
 
-      })
+    fs.writeFileSync(productsFilePath, JSON.stringify(newEditProduct), "utf-8");
 
-      fs.writeFileSync(productsFilePath, JSON.stringify(newEditProduct), "utf-8");
+    res.redirect("/");
+  },
+};
 
-      res.redirect("/");  
-    
-
-},
-
-}
-
-
-module.exports = mainControllers
+module.exports = mainControllers;
