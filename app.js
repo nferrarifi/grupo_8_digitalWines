c086631a9a3697feb7e6e1097deadf743a4f6625
nferrarifi <nahuel.ferrarifisicaro@gmail.com>
2022-05-03 15:30:29 -0300

    Adición de descuento a formulario de EDIT y fix de req.body en app.js

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ const app = express();
 const router = require("./routers/main");
 const PORT = process.env.PORT || 3050;
 const methodOverride = require("method-override");
-
-app.use(methodOverride("_method"));
-app.use(express.static("public"));
-app.use(router);
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+app.use(router);
 
 app.listen(PORT, () => console.log("servidor corriendo en el puerto 3050"));
